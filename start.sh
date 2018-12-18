62cc487f91f571baef1370a059a1d1a165ac97c9
Menna Hany <40391976+mennahany93@users.noreply.github.com>
2018-12-18 19:25:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/mennahany93/kubernetes-ruby_app.git
+#git clone https://github.com/mennahany93/kubernetes-ruby_app.git
 chmod +x init.sh
 docker build -t rubyimage .
 kubectl apply -f kubeconfigs
