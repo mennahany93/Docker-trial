7d069d42d5caacf54d52197c6a73fda85a0a8876
Menna Hany <40391976+mennahany93@users.noreply.github.com>
2018-12-19 12:50:36 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@
 #git clone https://github.com/mennahany93/kubernetes-ruby_app.git
 chmod +x init.sh
 docker build -t rubyimage .
-kubectl apply -f kubeconfigs
+kubectl apply -f kubeconfigs/postgres-deployment.yaml
+kubectl apply -f kubeconfigs/redis-deployment.yaml
+kubectl apply -f kubeconfigs/app-deployment.yaml
+
